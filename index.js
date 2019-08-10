7f1717806650e1cdd2d4c2553022c83f168b1389
Shaswat <shaswat178@gmail.com>
2019-08-10 11:15:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,96 @@
+'use strict'
+
+// Components
+import TaskCheck from './task-check'
+import TaskProject from './task-project'
+import TaskActions from './task-actions'
+import DragHandle from './../sortable/drag-handle'
+
+// Theme
+import { colors, typography } from './../../theme'
+
+const Task = ({ task, onMove, onDelete }) => {
+  const { title, description, project, type } = task
+  const desc =
+    description.length >= 30 ? `${description.substr(0, 50)}...` : description
+
+  return (
+    <li>
+      {type === 'done' ? <TaskCheck /> : null}
+
+      <div className="heading">
+        <div>
+          <h2>
+            {title} <TaskProject project={project} />
+          </h2>
+          <p>{desc}</p>
+        </div>
+
+        <div className="dragger">
+          <DragHandle />
+        </div>
+      </div>
+
+      {type !== 'done' ? ( // eslint-disable-line no-negated-condition
+        <div className="actions">
+          <TaskActions task={task} onMove={onMove} onDelete={onDelete} />
+        </div>
+      ) : 
+      <div className="actions">
+      <TaskActions task={task} onMove={onMove} onDelete={onDelete} />
+      </div>}
+      <style jsx>{`
+        li {
+          margin-bottom: 20px;
+          display: flex;
+          flex-wrap: wrap;
+        }
+
+        .heading {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          max-width: ${type === 'done' ? 'calc(280px - 37px)' : '100%'};
+          flex-basis: ${type === 'done' ? 'calc(280px - 37px)' : '100%'};
+        }
+
+        h2 {
+          font-weight: ${typography.bold};
+          font-size: ${typography.f16};
+          color: ${colors.white};
+          line-height: 1.5em;
+          word-wrap: break-word;
+        }
+
+        p {
+          color: ${colors.romanSilver};
+          line-height: 1.75;
+          font-size: ${typography.f12};
+          margin-top: 5px;
+          word-wrap: break-word;
+        }
+
+        .dragger {
+          flex-basis: 10px;
+          cursor: -webkit-grab;
+        }
+
+        .actions {
+          transform: translateY(-5px);
+          opacity: 0;
+          max-height: 0;
+          transition: 0.2s;
+          flex-basis: 100%;
+        }
+
+        li:hover .actions {
+          opacity: 1;
+          max-height: auto;
+          transform: translateY(0);
+        }
+      `}</style>
+    </li>
+  )
+}
+
+export default Task
---
+++ task-actions.js
@@ -0,0 +1,57 @@
+'use strict'
+
+// Packages
+import Link from 'next/link'
+
+// Theme
+import { colors, typography } from './../../theme'
+
+const TaskActions = ({ task, onDelete, onMove }) => {
+  const { id, type } = task
+  const isToday = type === 'today' ? 'done' : 'today'
+  const nextType = type === 'today' ? 'today' : 'backlog'
+  return (
+    <ul>
+      {type !== 'done' ? ( // eslint-disable-line no-negated-condition
+        <li onClick={() => onMove(nextType, task)}>{isToday}</li>
+      ) : null}
+
+      {type=='done'?(null):   
+      type !== 'backlog' ? ( // eslint-disable-line no-negated-condition
+        <li onClick={() => onMove('back', task)}>backlog</li>
+      ) : null}
+      
+      {type!=='done'?(
+      <li>
+      <Link href={`/edit?id=${id}`}>
+        <span>view</span>
+      </Link>
+      </li>):null}
+
+      <li onClick={() => onDelete(task)}>delete</li>
+
+      <style jsx>{`
+        li {
+          color: ${colors.white};
+          display: inline-block;
+          font-size: ${typography.f12};
+          font-weight: ${typography.semibold};
+          margin-right: 10px;
+          color: ${colors.romanSilver};
+          cursor: pointer;
+        }
+
+        span {
+          color: ${colors.romanSilver};
+        }
+
+        li:hover,
+        span:hover {
+          color: ${colors.white};
+        }
+      `}</style>
+    </ul>
+  )
+}
+
+export default TaskActions
